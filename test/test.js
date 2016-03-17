f8a07d72f7950c570559686fe6820c5e949ef774
Eleazar Díaz Delgado <eleazardzdo@gmail.com>
2016-03-17 09:26:14 +0000

    Addind test and coverage

--- test/test.js
+++ test/test.js
@@ -27,25 +27,36 @@ describe('Regex', function() {
 
     describe('Fahrenheit', function () {
         it('should match any substring of "Fahrenheit"', function () {
-            medida = new Kelvin();
-            assert.notEqual(false, medida.check("Kelvin"));
-            assert.notEqual(false, medida.check("Kel"));
-            assert.notEqual(false, medida.check("K"));
-            assert.notEqual(false, medida.check("k"));
-            assert.notEqual(false, medida.check("kElVi"));
+            medida = new Fahrenheit();
+            assert.notEqual(false, medida.check("Fah"));
+            assert.notEqual(false, medida.check("F"));
+            assert.notEqual(false, medida.check("Fahrenheit"));
+            assert.notEqual(false, medida.check("Fahren"));
         });
     });
 });
 
 describe('Conversions', function() {
-    describe('Celsius', function () {
-        var medida = new Celsius(0);
-        it('toFahrenheit"', function () {
+    describe('String Parameter', function () {
+        var medida = new Celsius("0 c");
+        it('String', function () {
             assert.equal(32, medida.toFahrenheit());
+        });
+    });
+    describe('Testing convert', function () {
+        var medida = Medida.convertir("0 c f");
+        it('convert 0 c f', function () {
+            assert.equal("32.00 toFahrenheit", medida);
+        });
+    });
+    describe('Celsius', function () {
+        var medid = new Celsius(0);
+        it('toFahrenheit"', function () {
+            assert.equal(32, medid.toFahrenheit());
         });
 
         it('toKelvin"', function () {
-            assert.equal(273, medida.toKelvin());
+            assert.equal(273, medid.toKelvin());
         });
     });
     describe('Kelvin', function () {
